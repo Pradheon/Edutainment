528566cd3394f6fea078e9b75c9dd5d93b7f3c1d
Joshan Rai <pradheon@gmail.com>
2022-03-03 12:58:20 -0800

    v1.2 Added: - Content to TutorialView

--- JoshanRai-Edutainment/TutorialView.swift
+++ JoshanRai-Edutainment/TutorialView.swift
@@ -9,7 +9,68 @@ import SwiftUI
 
 struct TutorialView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            VStack {
+                VStack {
+                    Text("How to Play")
+                        .font(.title)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(.thickMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    
+                    HStack {
+                        VStack(spacing: 10) {
+                            Text("1. Select 'New Game' from the game menu.")
+                                .font(.body)
+                                .padding(36)
+                                .frame(maxWidth: .infinity)
+                                .background(.regularMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                            Text("3. Use the picker to select the amount of questions to be asked.")
+                                .font(.body)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(.regularMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                        .fixedSize(horizontal: false, vertical: true)
+                        .frame(maxHeight: 300)
+                        VStack(spacing: 10) {
+                            Text("2. Use the stepper (+/- buttons) to select which multiplication tables to practice up to.")
+                                .font(.body)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(.regularMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                            Text("4. Press the 'Start Game' button at the bottom of 'New Game Menu'.")
+                                .font(.body)
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(.regularMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                        .fixedSize(horizontal: false, vertical: true)
+                        .frame(maxHeight: 300)
+                    }
+                    Text("When in-game, you will be asked a question like 'What is 6 x 8 ?'. \nSelect the correct answer to advance!")
+                        .font(.body)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(.regularMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    Text("Selecting the incorrect answer will move your progress back by one question and show you the correct answer.")
+                        .font(.body)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(.regularMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
+            }
+        }
     }
 }
 
