a1d575964e5a10981f85eacec7796b9224944500
Joshan Rai <pradheon@gmail.com>
2022-03-03 10:41:46 -0800

    Initial Commit

---
+++ JoshanRai-Edutainment/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JoshanRai-Edutainment
+//
+//  Created by Joshan Rai on 3/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoshanRai-Edutainment/JoshanRai_EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_EdutainmentApp.swift
+//  JoshanRai-Edutainment
+//
+//  Created by Joshan Rai on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
