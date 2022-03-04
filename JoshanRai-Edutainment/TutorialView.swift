402c81e7792d700aa33e76aee81c178515c82ec3
Joshan Rai <pradheon@gmail.com>
2022-03-04 02:01:48 -0800

    v2.5 Changes: - Minor updates to correct text in the Tutorial screen
    Log:
    - ProgressView bug unresolved

--- JoshanRai-Edutainment/TutorialView.swift
+++ JoshanRai-Edutainment/TutorialView.swift
@@ -7,6 +7,64 @@
 
 import SwiftUI
 
+struct RoundedRectangleRegularMaterial: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity)
+            .background(.regularMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+}
+
+extension View {
+    func roundedRectangleRegularMaterial() -> some View {
+        modifier(RoundedRectangleRegularMaterial())
+    }
+}
+
+struct RoundedRectangleThickMaterial: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity)
+            .background(.thickMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func roundedRectangleThickMaterial() -> some View {
+        modifier(RoundedRectangleThickMaterial())
+    }
+}
+
+struct BodyFontAndAutoPadding: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.body)
+            .padding()
+    }
+}
+
+extension View {
+    func bodyFontAndAutoPadding() -> some View {
+        modifier(BodyFontAndAutoPadding())
+    }
+}
+
+struct TitleFontAndAutoPadding: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .padding()
+    }
+}
+
+extension View {
+    func titleFontAndAutoPadding() -> some View {
+        modifier(TitleFontAndAutoPadding())
+    }
+}
+
 struct TutorialView: View {
     @Environment(\.dismiss) var dismiss
     
@@ -17,59 +75,51 @@ struct TutorialView: View {
             
             VStack {
                 VStack {
+                    Spacer()
+                    
                     Text("How to Play")
-                        .font(.title)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.thickMaterial)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .titleFontAndAutoPadding()
+                        .roundedRectangleThickMaterial()
                     
                     HStack {
                         VStack(spacing: 10) {
                             Text("1. Select 'New Game' from the game menu.")
                                 .font(.body)
                                 .padding(36)
-                                .frame(maxWidth: .infinity)
-                                .background(.regularMaterial)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .roundedRectangleRegularMaterial()
                             Text("3. Use the picker to select the amount of questions to be asked.")
-                                .font(.body)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(.regularMaterial)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .bodyFontAndAutoPadding()
+                                .roundedRectangleRegularMaterial()
                         }
                         .fixedSize(horizontal: false, vertical: true)
                         .frame(maxHeight: 300)
                         VStack(spacing: 10) {
                             Text("2. Use the stepper (+/- buttons) to select which multiplication tables to practice up to.")
-                                .font(.body)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(.regularMaterial)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .bodyFontAndAutoPadding()
+                                .roundedRectangleRegularMaterial()
                             Text("4. Press the 'Start Game' button at the bottom of 'New Game Menu'.")
-                                .font(.body)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(.regularMaterial)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .bodyFontAndAutoPadding()
+                                .roundedRectangleRegularMaterial()
                         }
                         .fixedSize(horizontal: false, vertical: true)
                         .frame(maxHeight: 300)
                     }
-                    Text("When in-game, you will be asked a question like 'What is 6 x 8 ?'. \nSelect the correct answer to advance!")
-                        .font(.body)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.regularMaterial)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
-                    Text("Selecting the incorrect answer will move your progress back by one question and show you the correct answer.")
-                        .font(.body)
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(.regularMaterial)
-                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    Text("How to Win")
+                        .titleFontAndAutoPadding()
+                        .roundedRectangleThickMaterial()
+                    VStack {
+                        Text("When in-game, you will be asked a question like 'What is 6 x 8 ?'. \nSelect the correct answer to advance and get a point!")
+                            .bodyFontAndAutoPadding()
+                            .roundedRectangleRegularMaterial()
+                        Text("Selecting the incorrect answer will decrease your score back by one point.")
+                            .bodyFontAndAutoPadding()
+                            .roundedRectangleRegularMaterial()
+                        Text("Get all of the answers correct to win!")
+                            .bodyFontAndAutoPadding()
+                            .roundedRectangleRegularMaterial()
+                    }
+                    Spacer()
                 }
             }
         }
